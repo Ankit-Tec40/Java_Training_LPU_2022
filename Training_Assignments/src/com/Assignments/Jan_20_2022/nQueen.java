ef0890d292f0599c8bcbe93380637cf654269cc7
Ankit Kumar <ankit.tec40@gmail.com>
2022-01-20 20:31:47 +0530

    Assignment 20jan,2022

--- Training_Assignments/src/com/Assignments/Jan_20_2022/nQueen.java
+++ Training_Assignments/src/com/Assignments/Jan_20_2022/nQueen.java
@@ -3,16 +3,47 @@ package com.Assignments.Jan_20_2022;
 import java.util.Scanner;
 
 public class nQueen {
+    static int [][] chess=new int[4][4];
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int [][] chess=new int[4][4];
-        System.out.println(" Enter Row No (0 to 3)");
-        int row=sc.nextInt();
-        System.out.println("Enter column no (0 to 3)");
-        int col=sc.nextInt();
 
+        for(int t=4;t>=0;t--) {
+            if(t==0){
+                System.out.println("All queens are Kept at suitable Positions");
+                break;
+            }
+            System.out.println(" Enter Row No (0 to 3)");
+            int row = sc.nextInt();
+            System.out.println("Enter column no (0 to 3)");
+            int col = sc.nextInt();
 
+            if(check(row, col)){
+                chess[row][col]=1;
+                System.out.println("Queen added at ("+row+","+col+")");
+            }
+            else{
+                System.out.println("You Can not put Queen at this position \nTry Diffrent Position in 4*4 chess Board");
+                t+=1;
+            }
 
+        }
 
     }
+
+    static Boolean check(int row, int col){
+        for(int r=0;r<4;r++){
+            if(chess[r][col]==1){
+                return false;
+            }
+        }
+        for(int c=0;c<4;c++){
+            if(chess[row][c]==1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
