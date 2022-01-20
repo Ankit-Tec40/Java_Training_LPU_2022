2083fe1d62f40aaa7b3ac4940e7f22ecc43b8042
Ankit Kumar <ankit.tec40@gmail.com>
2022-01-20 14:15:22 +0530

    Assignment 20jan,2022

---
+++ Training_Assignments/src/com/Assignments/Jan_20_2022/TernaryOperators.java
@@ -0,0 +1,9 @@
+package com.Assignments.Jan_20_2022;
+//Implement Ternary Operators if possible in above scenari (discount coupon)
+public class TernaryOperators {
+    public static void main(String[] args) {
+        int promoCode=123456;
+        int minAmount=500;
+
+    }
+}
