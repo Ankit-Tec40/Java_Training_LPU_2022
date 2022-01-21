3ad8ebd3d5c37c40beb72dc29cfc6a149dfa74a1
Ankit Kumar <ankit.tec40@gmail.com>
2022-01-21 22:12:00 +0530

    Assignment

--- Training_Assignments/src/com/Assignments/jan_21_2022/image.java
+++ Training_Assignments/src/com/Assignments/jan_21_2022/image.java
@@ -1,25 +1,72 @@
 package com.Assignments.jan_21_2022;
 
 public class image {
-//    //1
-//    static rotateRight(int[][][] arr){
-//
-//    }
-//
+
+    static void Print3DArray(int[][][] array){
+        for(int[][] a:array){
+            for(int [] b:a){
+                for (int c:b){
+                    System.out.print(c+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    //1
+    static void rotateRight(int[][][] arr){
+        int[][][] newarr1;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                for(int k=0;k<arr[i][j].length;k++){
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 //    //2
 //    static rotateLeft(int[][][] arr){
 //
 //    }
 //
-//    //3
-//    static grayScale(int[][][] arr){
-//
-//    }
+
+
+
+
+
+
+
+
+    //3
+    static void grayScale(int[][][] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                int sum=0;
+                for(int k=0;k<arr[i][j].length;k++){
+                    sum+=arr[i][j][k];
+                }
+                int[] sumA={sum/3};
+                arr[i][j]=sumA;
+            }
+        }
+        System.out.println("---------Grey scale-------");
+        Print3DArray(arr);
+
+    }
 
 
 
 
     public static void main(String[] args) {
-        int[][][] pixel={  };
+        int[][][] pixel={ {{25,47,96},{24,48,36},{12,78,24}},{{48,14,105},{45,200,48},{78,47,75}},{{72,98,65},{114,45,93},{125,68,27}} };
+        grayScale(pixel);
     }
 }
