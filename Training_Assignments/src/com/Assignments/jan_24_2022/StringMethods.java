60e726c931149ce2b7a3f25fd095129b1368af69
Ankit Kumar <ankit.tec40@gmail.com>
2022-01-24 22:09:00 +0530

    Assignment

--- Training_Assignments/src/com/Assignments/jan_24_2022/StringMethods.java
+++ Training_Assignments/src/com/Assignments/jan_24_2022/StringMethods.java
@@ -2,6 +2,14 @@ package com.Assignments.jan_24_2022;
 
 public class StringMethods {
     public static void main(String[] args) {
+        String name="My name Is Ankit Kumar  ";
+        System.out.println(name.charAt(3));
+        System.out.println(name.trim());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.indexOf('a'));
+        System.out.println(name.replace('A','M'));
+
 
     }
 }
