f595ed37c4383c055eb41e7834e5ceeca2448646
Ankit Kumar <ankit.tec40@gmail.com>
2022-01-24 22:16:43 +0530

    Assignment

--- Training_Assignments/src/com/Assignments/jan_24_2022/FilterFiles.java
+++ Training_Assignments/src/com/Assignments/jan_24_2022/FilterFiles.java
@@ -1,8 +1,22 @@
 package com.Assignments.jan_24_2022;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class FilterFiles {
     public static void main(String[] args) {
-        String[] file={"song.mp3","hello.jpg","get.txt","video.mp4","image.jpg","print.png","get.txt"};
+        String[] file={"song.mp3","hello.jpg","get.txt","video.mp4","image.jpg","print.png","table.txt"};
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter File type");
+        String fileType=sc.next();
+        for(String x:file){
+            String[] t=x.split("\\.");
+            if(t[1].equals(fileType)){
+                System.out.println(x);
+            }
+
+
+        }
 
     }
 }
--- Training_Assignments/src/com/Assignments/jan_24_2022/StringMethods.java
+++ Training_Assignments/src/com/Assignments/jan_24_2022/StringMethods.java
@@ -1,5 +1,7 @@
 package com.Assignments.jan_24_2022;
 
+import java.util.Arrays;
+
 public class StringMethods {
     public static void main(String[] args) {
         String name="My name Is Ankit Kumar  ";
@@ -9,7 +11,8 @@ public class StringMethods {
         System.out.println(name.toUpperCase());
         System.out.println(name.indexOf('a'));
         System.out.println(name.replace('A','M'));
-
+        String [] lst=name.split(" ");
+        System.out.println(Arrays.toString(lst));
 
     }
 }
