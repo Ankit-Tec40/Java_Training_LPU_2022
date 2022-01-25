5048deee3d44fab439857c276d71a304c4f3388b
Ankit Kumar <ankit.tec40@gmail.com>
2022-01-25 19:40:26 +0530

    Assignment

---
+++ Self_Practice/src/com/Classes/LeetCode/que14.java
@@ -0,0 +1,18 @@
+package com.Classes.LeetCode;
+
+public class que14 {
+    public static void main(String[] args) {
+        String[] strs = {"dog","racecar","car"};
+        int minl=200;
+        for(String x:strs){
+            if(x.length()<minl){
+                minl=x.length();
+            }
+        }
+        String res="";
+        char curr;
+
+
+
+    }
+}
---
+++ Training_Assignments/src/com/Assignments/jan_25_2022/MutableStrings.java
@@ -0,0 +1,9 @@
+package com.Assignments.jan_25_2022;
+
+public class MutableStrings {
+    public static void main(String[] args) {
+        StringBuffer str1=new StringBuffer("My Name Is Ankit kumar");
+        StringBuilder str2=new StringBuilder("I Am From Bihar India");
+
+    }
+}
