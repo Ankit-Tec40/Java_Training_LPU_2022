35c9f82880f76a603e3280937e38b31c2aedf796
Ankit Kumar <ankit.tec40@gmail.com>
2022-01-25 19:47:38 +0530

    Assignment

--- Training_Assignments/src/com/Assignments/jan_25_2022/MutableStrings.java
+++ Training_Assignments/src/com/Assignments/jan_25_2022/MutableStrings.java
@@ -3,7 +3,17 @@ package com.Assignments.jan_25_2022;
 public class MutableStrings {
     public static void main(String[] args) {
         StringBuffer str1=new StringBuffer("My Name Is Ankit kumar");
-        StringBuilder str2=new StringBuilder("I Am From Bihar India");
+        StringBuilder str2=new StringBuilder("My Name Is Ankit kumar");
+
+        System.out.println("--------------String Buffer-------------");
+        System.out.println(str1.append(234));
+        System.out.println(str1.capacity());
+        System.out.println(str1.reverse());
+
+        System.out.println("-------------String Builder-------------");
+        System.out.println(str2.append(245));
+        System.out.println(str2.capacity());
+        System.out.println(str2.reverse());
 
     }
 }
