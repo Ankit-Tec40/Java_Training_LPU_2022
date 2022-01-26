8d9158fba7eff4ca92ec16c65ed46e9e6f5554bf
Ankit Kumar <ankit.tec40@gmail.com>
2022-01-26 16:49:04 +0530

    practice

---
+++ Self_Practice/src/com/Classes/Map/Map.java
@@ -0,0 +1,24 @@
+package com.Classes.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class map {
+    public static void main(String[] args) {
+        Map <Integer,Integer> mp=new HashMap<>();
+        mp.put(1,456);
+        mp.put(5,785);
+        mp.put(6,785);
+        mp.put(9,897);
+
+
+        System.out.println(mp);
+
+        for(Map.Entry<Integer,Integer> e:mp.entrySet()){
+            System.out.println(e);
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+    }
+}
